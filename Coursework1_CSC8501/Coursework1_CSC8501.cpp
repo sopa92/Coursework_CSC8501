99e294475619878ddb27f7fe18312ff81dcbd224
Sofia Papadopoulou <sopa92@hotmail.com>
2019-10-17 14:06:26 +0100

    15-puzzle configurations generator created

--- Coursework1_CSC8501/Coursework1_CSC8501.cpp
+++ Coursework1_CSC8501/Coursework1_CSC8501.cpp
@@ -10,29 +10,33 @@
 
 using namespace std;
 
-
+int GetInputNumber();
 int GetInputNumber(Puzzle* puzzleArr);
 bool isExistingNumber(int givenInput, Puzzle* puzzleArr);
-void SettingValuesManually(Puzzle* puzzle);
+void SettingValuesManually(Puzzle* puzzle, int rows, int cols);
+void SettingValuesAuto(Puzzle* puzzle, int rows, int cols);
+void CreatePuzzleConfigurationsRandomly();
 
 int main()
 {
 	Puzzle mainPuzzle;
-	SettingValuesManually(&mainPuzzle);
+	int rows = (&mainPuzzle)->get_hor_size();
+	int columns = (&mainPuzzle)->get_vert_size();
+	SettingValuesManually(&mainPuzzle, rows, columns);
+	//SettingValuesAuto(&mainPuzzle, rows, columns);
+	cout << mainPuzzle << endl ;
 
-	cout << mainPuzzle;
+	CreatePuzzleConfigurationsRandomly();
 	return 0;
 }
 
-void SettingValuesManually(Puzzle* puzzle) {
+void SettingValuesManually(Puzzle* puzzle, int rows, int cols) {
 	cout << "Please give me 15 numbers between 1 and 20." << endl;
 	cout << "| 01 | 02 | 03 | 04 | 05 | 06 | 07 | 08 | 09 | 10 | 11 | 12 | 13 | 14 | 15 |" << endl;
 	cout << "----------------------------------------------------------------------------" << endl;
 	string printedArray = "| __ | __ | __ | __ | __ | __ | __ | __ | __ | __ | __ | __ | __ | __ | __ |";
 	string unfilledGap = "__";
-	cout << printedArray << endl;
-	int rows = puzzle->get_hor_size();
-	int cols = puzzle->get_vert_size();
+	cout << printedArray << endl;	
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < cols; ++j) {
 			if (i == rows - 1 && j == cols - 1) {
@@ -42,15 +46,64 @@ void SettingValuesManually(Puzzle* puzzle) {
 			puzzle->set_element(i, j, GetInputNumber(puzzle));
 			string newElement = std::to_string(puzzle->get_element(i, j));
 			printedArray.replace(printedArray.find(unfilledGap), unfilledGap.length(), newElement);
+			system("cls");
 			cout << printedArray << endl;
 		}
 	}
 	cout << "Thank you! The 15-puzzle is now ready!" << endl;
 }
 
+void SettingValuesAuto(Puzzle* puzzle, int rows, int cols) {
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			if (i == rows - 1 && j == cols - 1) {
+				puzzle->set_element(i, j, 0);
+				break;
+			}
+			int rndNumber = rand() % 20;
+			while (isExistingNumber(rndNumber, puzzle)) {
+				rndNumber = rand() % 20;
+			}
+			puzzle->set_element(i, j, rndNumber);
+			//cout << puzzle->get_element(i, j) << "\t";
+		}
+		//cout << endl;
+	}
+}
+
+void CreatePuzzleConfigurationsRandomly() {
+	cout << "How many 15-puzzle configurations do you want me to create?" << endl;
+	int inputAmount = GetInputNumber();
+	for (int i = 0; i < inputAmount; ++i) {
+		Puzzle randPuzzle;
+		SettingValuesAuto(&randPuzzle, randPuzzle.get_hor_size(), randPuzzle.get_vert_size());
+		cout << "Puzzle " << i+1 << endl << randPuzzle << "------------------------------" << endl;
+	}
+}
+
+int GetInputNumber() {
+	int givenInput = 0;
+	cout << endl << "Type your chosen number and press Enter:" << endl;
+	cin >> givenInput;
+	while (true) {
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "This is a character. Please choose a number." << endl;
+			cin >> givenInput;
+		}
+		else {
+			break;
+		}
+	}
+	return givenInput;
+}
 
 int GetInputNumber(Puzzle* puzzleArr) {
 	int givenInput = 0;
+	cout << endl << "Type your chosen number and press Enter:" << endl;
+	cin >> givenInput;
 	while (true) {
 		if (cin.fail())
 		{
@@ -64,7 +117,7 @@ int GetInputNumber(Puzzle* puzzleArr) {
 			cin >> givenInput;
 		}
 		else if (isExistingNumber(givenInput, puzzleArr)) {
-			cout << "You must not choose an existing number. \n Please choose another number between 1 and 20." << endl;
+			cout << "You must not choose an existing number. \nPlease choose another number between 1 and 20." << endl;
 			cin >> givenInput;
 		}
 		else {
